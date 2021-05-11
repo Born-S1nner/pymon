bd7e26e511e37a094ae163cce74d9a3639e24a1b
123 <cookie.twenty01@gmail.com>
2021-05-11 18:03:08 -0400

    relearn to function with id

--- daftpunk/homework/models.py
+++ daftpunk/homework/models.py
@@ -4,15 +4,15 @@ from django.db import models
 class Song(models.Model):
   title = models.CharField(max_length=200)
   composer = models.CharField(max_length=200)
-  likes = models.IntegerField(default=0)
 
   def __str__(self):
-    return self.title, self.composer
+    return '{} {}'.format(self.title, self.composer)
 
 class SoundBass(models.Model):
   song = models.ForeignKey(Song, on_delete=models.CASCADE)
   insturment = models.CharField(max_length=300)
-  
+  tempo = models.IntegerField(default=0)
+
   def __str__(self):
     return self.insturment
 
--- daftpunk/homework/views.py
+++ daftpunk/homework/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Song, SoundBass
 
 # Create your views here.
 
-def index(res): 
-  return HttpResponse("Around the World")
+def index(res, id): 
+  s = Song.objects.get(id=id)
+  return HttpResponse("%s" % s.title)
 
 def home(res): 
   return HttpResponse("DaftPunk still lives")
