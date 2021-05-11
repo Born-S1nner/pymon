86d7531b37b28ff2fe5c1dc2190b3f72e82a71b0
123 <cookie.twenty01@gmail.com>
2021-05-11 16:53:36 -0400

    small mistake chaneg

--- daftpunk/homework/models.py
+++ daftpunk/homework/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+  title = models.CharField(max_length=200)
+  composer = models.CharField(max_length=200)
+  likes = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.title, self.composer
+
 class SoundBass(models.Model):
+  song = models.ForeignKey(Song, on_delete=models.CASCADE)
   insturment = models.CharField(max_length=300)
   
   def __str__(self):
     return self.insturment
-
-class Song(models.Model):
-  soundbass = models.ForeignKey(SoundBass, on_delete=models.CASCADE)
-  title = models.CharField(max_length=200)
-  likes = models.IntegerField(default=0)
-
-  def __str__(self):
-    return self.title
 
