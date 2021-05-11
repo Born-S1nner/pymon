f26eb8806e697333920da301b05105e10ff6943e
123 <cookie.twenty01@gmail.com>
2021-05-11 16:20:29 -0400

    migration

--- daftpunk/homework/models.py
+++ daftpunk/homework/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class SoundBass(models.Model):
+  insturment = models.CharField(max_length=300)
+  
+  def __str__(self):
+    return self.insturment
+
+class Song(models.Model):
+  soundbass = models.ForeignKey(SoundBass, on_delete=models.CASCADE)
+  title = models.CharField(max_length=200)
+  likes = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.title
+
