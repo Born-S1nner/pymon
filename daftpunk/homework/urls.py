9f64f355bfc4e9eb1884e3f20a100711d09af5c4
123 <cookie.twenty01@gmail.com>
2021-05-12 15:04:28 -0400

    playing with url and views

--- daftpunk/homework/urls.py
+++ daftpunk/homework/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("<int:id>/", views.index, name="index"),
+  path("<int:id>/songinsta/", views.song_insta, name="sing_pick"),
+  path("<int:id>/songpick/", views.song_pick, name="sing_pick"),
+  path("index/", views.index, name="index"),
   path("", views.home, name="home"),
 ]
--- daftpunk/homework/views.py
+++ daftpunk/homework/views.py
@@ -4,10 +4,17 @@ from .models import Song, SoundBass
 
 # Create your views here.
 
-def index(res, id): 
+def index(res): 
+  s = Song.objects.all()
+  return HttpResponse("<div>%s</div>" %(s))
+
+def song_pick(res, id):
   s = Song.objects.get(id=id)
-  sb = s.soundbass_set.get(id=1)
-  return HttpResponse("<h1>%s</h1><br/><h2>%s</h2>" %(s.title, sb.insturment))
+  return HttpResponse("<h1>%s</h1><br/><h2>By: %s</h2><hr/>" %(s.title, s.composer))
+
+def song_insta(res, id):
+  sb = SoundBass.objects.get(id=id)
+  return HttpResponse("<p>Composer: %s</p><p>Tempo: %s</p>" %(sb.insturment, sb.tempo))
 
 def home(res): 
   return HttpResponse("DaftPunk still lives")
