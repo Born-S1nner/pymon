791dabf42bd02d945a354cd5876e2b1e57464661
123 <cookie.twenty01@gmail.com>
2021-05-13 17:50:19 -0400

    form create

---
+++ daftpunk/homework/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class CreateNewSong(forms.Form):
+  title = forms.CharField(label="title", max_length=300)
+  composer = forms.CharField(label="composer", max_length=200)
+        
--- daftpunk/homework/views.py
+++ daftpunk/homework/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Song, SoundBass
+from .forms import CreateNewSong
 
 # Create your views here.
 
@@ -21,7 +22,8 @@ def song_insta(res, id):
   )
 
 def create(res):
-  return render(res, "homework/create.html", {})
+  form = CreateNewSong()
+  return render(res, "homework/create.html", {"form":form})
 
 def home(res): 
   return render(res, "homework/home.html", {})
